496173b04cfd2589c8b35db453624990deae8730
Alvaro Kú <alvaroku123@gmail.com>
2022-08-26 09:48:31 -0500

    ajustes de bd

--- CarouselImagesApp/Migrations/20220826142538_001.Designer.cs
+++ CarouselImagesApp/Migrations/20220826142538_001.Designer.cs
@@ -3,14 +3,16 @@ using CarouselImagesApp.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CarouselImagesApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220826142538_001")]
+    partial class _001
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
